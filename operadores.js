9b303233c13a36e5afa75cc075983965ffac4f5d
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 18:17:29 -0500

    operadores-Qoukka

---
+++ operadores.js
@@ -0,0 +1,22 @@
+let a, b, c, d, e, f;
+//suma
+a = 5 +5;
+console.log(a); 
+//resta
+b = 15 - 5;
+console.log(b);
+//multiplicacion
+c = 5 * 5;
+console.log(c);
+//division
+d = 50 / 5;
+console.log(d);
+//modulo
+e = 11 % 2;
+console.log(e);
+//incremento
+f = ++b;
+console.log(f);
+//decremento    
+f = --b;
+console.log(f);
