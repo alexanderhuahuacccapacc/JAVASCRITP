a5642c99d7103d29ea27d58f319a9e4e4a8715ed
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 16:50:25 -0500

    Variables-Qoukka

---
+++ Variblesqoukka.js
@@ -0,0 +1,9 @@
+var miVariable = "Hello, World!";
+console.log(miVariable);
+
+let numero = 42;
+console.log(numero);
+// this is a comment "let es el mas recomendado que var"
+
+console.log("Hello, World!..");
+console.log("This is a sample JavaScript file.");
---
+++ variables.js
@@ -0,0 +1,6 @@
+var miVariable = "Hello, World!";
+console.log(miVariable);
+
+let numero = 42;
+console.log(numero);
+// this is a comment "let es el mas recomendado que var"
