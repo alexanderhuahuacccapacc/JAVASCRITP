2371f614da48a231f97b797bc1080f6b341bdf38
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 17:56:47 -0500

    HoisTing-Qoukka

---
+++ Hoistting.js
@@ -0,0 +1,18 @@
+// hohsting
+console.log(miVariable);
+var miVariable = "Hello, World!";
+// l princio de hoisting es poder declarar variables y funciones despues de usarlas y el interprete de javascript las mueve al inicio del codigo
+// pero solo con var y function
+// con let y const no funciona el hoisting..
+// ejemplo con let
+let numero = 42;
+console.log(numero); // esto funciona porque la variable ya fue declarada antes de usarla
+// pero si intentamos usarla antes de declararla
+
+//console.log(numero2); // ReferenceError: Cannot access 'numero2' before initialization
+let numero2 = 43;
+
+// con const pasa lo mismo
+console.log(numero3); // ReferenceError: Cannot access 'numero3' before initialization
+const numero3 = 44; 
+// por lo tanto es recomendable usar let y const en lugar de var para evitar problemas con el hoisting.
