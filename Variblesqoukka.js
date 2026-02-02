48d2fa2c577f3c0084e90e2ba02499dd840f0444
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 17:05:47 -0500

    Variables-Qoukka

--- Variblesqoukka.js
+++ Variblesqoukka.js
@@ -13,3 +13,17 @@ console.log(mensaje_prueba);
 // "tambien se pueden usar guiones bajos en las variables conocidas como snake_case"
 let $numero = 42;
 console.log($numero); //"tambien se puede usar el signo de dolar"
+
+// "no se recomienda usar var, ya que tiene un alcance de funcion y puede generar errores inesperados"
+
+// "let y const tienen un alcance de bloque, lo que los hace mas seguros para usar en la mayoria de los casos"
+const PI = 3.1416;
+console.log(PI);
+// "const se usa para valores que no cambian, mientras que let se usa para valores que pueden cambiar"
+const GRAVEDAD = 9.81;
+console.log(GRAVEDAD);
+// JavaScript es un lenguaje sencible a mayusculas y minusculas
+let nombre = "Juan";
+let Nombre = "Pedro";
+console.log(nombre);
+console.log(Nombre);
