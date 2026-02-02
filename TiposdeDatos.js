684463de75eb7840a783351f009a4639f27a5081
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 17:13:43 -0500

    Tiposdedatos-Qoukka

---
+++ TiposdeDatos.js
@@ -0,0 +1,21 @@
+// tipo de dato numero
+const Number = 42;
+console.log(Number);
+// tipo de dato texto
+const String = "This is a string.";
+console.log(String);
+// tipo de dato booleano
+const Boolean = true;
+console.log(Boolean);
+// tipo de dato nulo
+const Null = null;
+console.log(Null);
+// tipo de dato indefinido
+let Undefined;
+console.log(Undefined);
+// tipo de dato objeto
+const Object = { nombre: "Juan", edad: 30 };    
+console.log(Object);
+// tipo de dato arreglo
+const Array = [1, 2, 3, 4, 5];
+console.log(Array);       
