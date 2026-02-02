ece03eeee9ac598b766ecb8007d386953af50ec3
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 16:59:17 -0500

    Variables-Qoukka

--- Variblesqoukka.js
+++ Variblesqoukka.js
@@ -7,3 +7,9 @@ console.log(numero);
 
 console.log("Hello, World!..");
 console.log("This is a sample JavaScript file.");
+
+let mensaje_prueba = "This is a sample JavaScript file.";
+console.log(mensaje_prueba);
+// "tambien se pueden usar guiones bajos en las variables conocidas como snake_case"
+let $numero = 42;
+console.log($numero); //"tambien se puede usar el signo de dolar"
