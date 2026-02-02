b2da0e6771af95d9ede75257a1dfdbdc0b4ec7b1
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 17:24:23 -0500

    Tiposdedatos-Qoukka

--- TiposdeDatos.js
+++ TiposdeDatos.js
@@ -1,3 +1,5 @@
+//ojo usamos const porque no vamos a cambiar el valor de las variables
+
 // tipo de dato numero
 const Number = 42;
 console.log(Number);
@@ -19,3 +21,26 @@ console.log(Object);
 // tipo de dato arreglo
 const Array = [1, 2, 3, 4, 5];
 console.log(Array);       
+
+// ahora usando let para variables que pueden cambiar
+// tipo de dato numero
+let miNumber = 43;
+console.log(miNumber);
+// tipo de dato texto
+const miString = "This is a string con let.";
+console.log(miString);
+// tipo de dato booleano
+const miBoolean = true;
+console.log(miBoolean);
+// tipo de dato nulo
+const miNull = null;
+console.log(miNull);
+// tipo de dato indefinido
+let miUndefined;
+console.log(miUndefined);
+// tipo de dato objeto
+const miObject = { nombre: "Juana", edad: 35 };    
+console.log(miObject);
+// tipo de dato arreglo
+const miArray = [1, 2, 3, 4, 5, 6];
+console.log(miArray);       
