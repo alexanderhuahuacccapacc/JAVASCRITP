5a1c451dcb4e075d76d19d57de7568e3aeec1cf9
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-02-02 17:26:57 -0500

    Tiposdedatos-Qoukka

--- TiposdeDatos.js
+++ TiposdeDatos.js
@@ -26,21 +26,33 @@ console.log(Array);
 // tipo de dato numero
 let miNumber = 43;
 console.log(miNumber);
+console.log(typeof miNumber);
 // tipo de dato texto
 const miString = "This is a string con let.";
 console.log(miString);
+console.log(typeof miString);
 // tipo de dato booleano
 const miBoolean = true;
 console.log(miBoolean);
+console.log(typeof miBoolean);
+
+
 // tipo de dato nulo
 const miNull = null;
 console.log(miNull);
+console.log(typeof miNull);
+// Ojo: typeof null en JavaScript devuelve "object", es un comportamiento historico del lenguaje    
+
+
 // tipo de dato indefinido
 let miUndefined;
 console.log(miUndefined);
+console.log(typeof miUndefined);
 // tipo de dato objeto
 const miObject = { nombre: "Juana", edad: 35 };    
 console.log(miObject);
+console.log(typeof miObject);
 // tipo de dato arreglo
 const miArray = [1, 2, 3, 4, 5, 6];
-console.log(miArray);       
+console.log(miArray);   
+console.log(typeof miArray);
